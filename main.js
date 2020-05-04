ddfa54d48cb2d9b64b40361950fa33e430ec1e8b
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-05-04 09:11:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,56 @@
 var canvas, clockWidth, cenX, cenY, clock;
 
-function startClock(cw) {
+
+startClock(360);
+
+
+function startClock(theWidth) {
   canvas = document.querySelector('canvas');
   clock = canvas.getContext('2d');
-  clockWidth = cw;
+  clockWidth = theWidth;
   //Assigning the center of the Canvas to a value
   cenX = canvas.height/2;
   cenY = canvas.width/2;
   
-  activateClock();
-  window.setInterval(activateClock, 1000);
+  drawTime();
+  window.setInterval(drawTime, 1000);
 }
 
 
-window.onload = function(){
-  startClock(350);
-}
 
-function activateClock() {
-  //Assigning cuurent date to a variable
-  var date = new Date();
+
+
+function drawTime() {
+
   clock.clearRect(0,0,canvas.width, canvas.height);
   
   //Drawing the Circle
   drawCircle();
+  assignHands();
+}
+
+    
+function assignHands() {
+  //Assigning cuurent date to a variable
+  var date = new Date();
+
   
   //Draw the hands and assign to the current time
   var hr = date.getHours();
   clock.strokeStyle = "black";
-  clock.lineWidth = 8
-  drawHands(clockWidth/3, hr * 30);
+  drawHands(clockWidth/3, hr * 30,8);
+  
   
   var min = date.getMinutes();
   clock.strokeStyle = "#c6c6cc";
-  clock.lineWidth = 6
-  drawHands(200, min * 6);
+  drawHands(200, min * 6,6);
   
   
   var sec = date.getSeconds();
   clock.strokeStyle = "red";
-  clock.lineWidth = 2
-  drawHands(clockWidth/2, sec * 6);
-};
+  drawHands(clockWidth/2, sec * 6,2);
+}
+
 
   
   
@@ -49,25 +58,23 @@ function activateClock() {
     //Draw the Inner Smaller Circle
    clock.beginPath();
    clock.arc(cenX,cenY,10,0, 2 * Math.PI ,false);
-   clock.strokeStyle = "black"
+   clock.strokeStyle = "blue"
    clock.fill();
    clock.closePath();
 
-   drawNumbers();
+   getNumber();
   };
   
   
-  function drawNumbers() {
-
-    var i =12;
+  function getNumber() {
     clock.strokeStyle = "black";
     clock.lineWidth = 3;
     
-    while(i>0){
+    for(num=1; num<13; num++){
       clock.save();
       clock.beginPath();
       clock.translate(cenX,cenY);
-      var angle = (i * 30) * Math.PI/180;
+      var angle = (num * 30) * Math.PI/180;
       clock.rotate(angle);
       clock.translate(0,-clockWidth/2);
 
@@ -77,7 +84,7 @@ function activateClock() {
       
       //Adding Numbers as the loop runs
       clock.font = '40px Monospace';
-      clock.fillText(i,-16,14);
+      clock.fillText(num,-16,14);
       clock.restore();
       
       //Draw a line for each number 
@@ -86,20 +93,23 @@ function activateClock() {
       clock.stroke();
       clock.closePath();
       clock.restore();
-      
-      i--
+  
+  
     } 
   }
   
-  function drawHands(length,angle) {
+  function drawHands(length,angle,lwidth) {
   //Draw line fuction which was later called at the top to draw the hands
   clock.save();
   clock.beginPath();
+  clock.lineWidth = lwidth;
+  clock.lineCap = "round"
   clock.translate(cenX,cenY);
   clock.rotate(-180 * Math.PI / 180);
   clock.rotate(angle * Math.PI / 180);
   clock.moveTo(0,-30);
   clock.lineTo(0,length);
+
   clock.stroke();
   clock.strokeStyle = "black";
   clock.closePath();
